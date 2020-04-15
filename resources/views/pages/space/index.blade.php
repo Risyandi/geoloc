93345e46fe3784d944f2b9a8c2bd9e25693012a0
Risyandi <Risyandi.94@gmail.com>
2020-04-15 20:31:26 +0700

    modified tags call the components

--- resources/views/pages/space/index.blade.php
+++ resources/views/pages/space/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="container">
-    <x-space></x-space>
+    <x-space />
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -15,7 +15,7 @@
                     </div>
                     @endif
 
-                    This is a space!
+                    This is a space pages!
                 </div>
             </div>
         </div>
